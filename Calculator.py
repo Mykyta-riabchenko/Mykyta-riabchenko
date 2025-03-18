3c7ebd4a626d78b9f35999e7c3dd3ad5f1a3ab6b
radomir <76589808+radomirklimov@users.noreply.github.com>
2025-03-18 20:53:41 +0100

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,13 @@
+class Calculator():
+
+  def __count(list):
+    sum = 0
+    for i in list:
+      sum += i
+    return sum
+
+  def getListOfMoney(amountOfMonety):
+    changedMoney = []
+    while __count(changedMoney) != amountOfMonety:
+      changedMoney.append(10)
+    return changedMoney
