1d06ccf3bcb1f858f954106963147a91ae22f3c3
radomir <76589808+radomirklimov@users.noreply.github.com>
2025-03-19 14:17:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-import Bank, Money
+from Bank import Bank
+from Calculator import Calculator
+from Money import Money
 bank = Bank()
-while 1:
-  cin = input("Input > ")
-  if(cin.isdigit()):
-    bank.addMoney(Money(int(cin)))
+calculator = Calculator()
+
+bank.addMoney(Money(1))
+bank.addMoney(Money(5))
+bank.addMoney(Money(5))
+bank.addMoney(Money(10))
+
+
+givenList = [1, 1, 5, 10]
+result = calculator.exchangeMoney(givenList, bank)
+
+print("Exchanged money:", result)
