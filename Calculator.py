27116ebeda4945c584f33f0cac3746f1882227ca
radomir <76589808+radomirklimov@users.noreply.github.com>
2025-03-19 14:13:33 +0100

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,18 +1,27 @@
-import Bank
-
+from Bank import Bank
 class Calculator():
-
-  def __count(self, list):
-    sum = 0
-    for i in list:
-      sum += i
-    return sum
-
+  
+  def _bankSize(self, bank: Bank):
+     bankSize = sum(bank.getCountOfMoney(n) * n for n in bank.getNominals() if isinstance(n, int))
+     return bankSize
+  
   def exchangeMoney(self, inputedMoney: list, bank: Bank):
     changedMoney = []
-    inputedSum = self.__count(inputedMoney)
-    for i in bank.getNominals():
-      while  bank.getCountOfMoney(i) <= inputedSum:
-        changedMoney =+ i 
-        inputedSum =- i
+    inputedSum = sum(inputedMoney)
+    if inputedSum > self._bankSize(bank):
+      return -1             #if given sum is larger then amount of money in the bank: return -1
+    for i in [100, 50,20,10,5,2,1]:
+        
+      while  i <= inputedSum and bank.getCountOfMoney(i) != 0:
+        changedMoney.append(i) 
+        inputedSum -= i
+        bank.withdrawMoney(i)
     return changedMoney
+ 
+     #TODO:if give 100 it has to return smaller money
+     #TODO:if the amount of needed coins is not enought the prog's ignoring it
+          #example bank[1, 5, 5, 10] givenList[1, 1, 5, 10]
+
+
+
+
