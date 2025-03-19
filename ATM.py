60975d48e8ebc0e08580d63a62324146aef09096
radomir <76589808+radomirklimov@users.noreply.github.com>
2025-03-19 14:16:26 +0100

    Create ATM.py

---
+++ ATM.py
@@ -0,0 +1,8 @@
+class ATM():
+  def __init__(self):
+    self.bank = Bank()
+    self.culculator = Culculator()
+  def start(self):
+     keep = True
+     while keep:
+        pass #do input output 
